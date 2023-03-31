78c1c8eae13d5ba3f0731df98d7ef9ed23a32883
DavidSichertech <davidhernandez@sichertech.com.mx>
2023-03-31 17:50:34 -0600

    listar casas en landing

---
+++ mg/scripts/js/listar_casas.js
@@ -0,0 +1,40 @@
+listarCasas()
+
+var idRegistro
+
+function listarCasas(){
+    //Buscamos los datos de la casa a editar
+    idRegistro = $(this).attr("data-id")
+    $.ajax({
+        url:"scripts/php/admin_nueva_casa.php",
+        cache: false,
+        data: {comm:"listarCasas", idReg:idRegistro},
+        dataType: "json",
+        method: "POST"
+    }).done(function(rest){
+        $.each(rest.data, function (i, item) {
+            // $("#idReg").val(item.id)
+            // $("#txtNombreCasa").val(item.name_house)
+            // $("#txtDireccion").val(item.address_house)
+            // $("#txtDescripcion").val(item.description)
+            // $("#txtTamanoPropiedad").val(item.property_size)
+            // $("#txtPrecio").val(item.price)
+            // $("#cboTipoCasa").val(item.type_house)
+            // $("#txtTamanoPlano").val(item.flat_size)
+            // $("#cboEstadoCasa").val(item.status_house)
+            
+            // if(item.src_image != ""){
+            //     $("#imgUpload").attr("src", item.src_image)
+            //     $("#txtImage_house").val(item.image_house)
+            //     $("#uploadImage").hide()
+            //     $("#imageHouse").show()
+            // }else{
+            //     $("#imgUpload").attr("src", "")
+            //     $("#txtImage_house").val("")
+            //     $("#uploadImage").show()
+            //     $("#imageHouse").hide()
+            // }
+        })
+    })
+}
+
