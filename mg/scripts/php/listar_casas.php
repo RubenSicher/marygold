62c558a6ba0f363afdf2a1253b4c5da2d20df668
DavidSichertech <davidhernandez@sichertech.com.mx>
2023-04-12 17:42:38 -0600

    Lista de casas por modelo

--- mg/scripts/php/listar_casas.php
+++ mg/scripts/php/listar_casas.php
@@ -1,13 +1,14 @@
 <?php
 
 $comm = $_POST["comm"];
+$modelo = $_POST["modelo"];
 
 if ($comm == 'listarCasas'){
 
     try{
 
         include_once "../../../dashboard/scripts/php/conectar.php";
-        $datos = $conn->query("SELECT id, name_house, address_house, description_house, property_size, price, type_house, flat_size, status_house, estado, image_house FROM admin_casas WHERE estado='1' LIMIT 5 ");
+        $datos = $conn->query("SELECT id, name_house, address_house, description_house, property_size, price, type_house, flat_size, status_house, estado, image_house FROM admin_casas WHERE estado='1' and modelo='$modelo' ");
         
         if($datos->num_rows >= 0){
             $data = array();
